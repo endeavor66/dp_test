cde571ed312983d4eb9aece0c20265365c562296
mwg <mwg@whut.edu.com>
2022-02-08 16:36:05 +0800

    修改dppresent,dpdatamanager,forecast,filemanager中url为宿主机ip

--- dp_test_provider_datamanager_8001/src/main/java/com/example/dp/ProviderApi/ForecastProviderApi.java
+++ dp_test_provider_datamanager_8001/src/main/java/com/example/dp/ProviderApi/ForecastProviderApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@FeignClient(name = "dp-test-provider-Forecast", url ="172.19.241.210:8001")
+@FeignClient(name = "dp-test-provider-Forecast", url ="172.19.241.99:8002")
 public interface ForecastProviderApi {
 
     @GetMapping("/DailyRenew")
