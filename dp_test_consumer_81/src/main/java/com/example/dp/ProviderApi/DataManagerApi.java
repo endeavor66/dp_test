eb961d71ab63fc974426fc72e293aaa6a7a89adc
mwg <mwg@whut.edu.com>
2022-02-10 15:05:18 +0800

    修改consumer, forecast两个微服务的DataManagerApi和application.yml

--- dp_test_consumer_81/src/main/java/com/example/dp/ProviderApi/DataManagerApi.java
+++ dp_test_consumer_81/src/main/java/com/example/dp/ProviderApi/DataManagerApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@FeignClient(name = "dp-test-provider-datamanager", url ="172.19.241.210:8001")
+@FeignClient(name = "dp-test-provider-datamanager", url ="172.19.241.99:8001")
 public interface DataManagerApi {
 
     @PostMapping("/shop/add")
--- dp_test_provider_forecast_8002/src/main/java/com/example/dp/ProviderApi/DataManagerApi.java
+++ dp_test_provider_forecast_8002/src/main/java/com/example/dp/ProviderApi/DataManagerApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 //@FeignClient(name = "dp-test-provider-datamanager", url ="172.19.241.210:8001")
-@FeignClient(name = "dp-test-provider-datamanager", url ="localhost:8001")
+@FeignClient(name = "dp-test-provider-datamanager", url ="172.19.241.99:8001")
 public interface DataManagerApi {
 
     @GetMapping("/shop/list")
